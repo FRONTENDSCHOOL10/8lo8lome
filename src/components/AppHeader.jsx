b0037c0825a83f598e8698eacb7c720ae3841a16
heejae <leeheejaelcode@gmail.com>
2024-09-01 13:02:47 +0900

    [기능] AppHeader 수정
    - 뒤로 가기 버튼 추가
    - children 추가

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,9 +1,24 @@
 import { memo } from 'react';
+import { node } from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
-function AppHeader() {
+AppHeader.propTypes = {
+  children: node.isRequired,
+};
+
+function AppHeader({ children }) {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <header className="flex justify-center py-3 bg-black ">
-      <h1 className="text-white">로고 영역</h1>
+    <header className="flex p-6 bg-subBg font-bold text-[20px]  w-full ">
+      <button type="button" onClick={handleGoBack}>
+        {'<'}
+      </button>
+      <h1 className="text-white mx-auto">{children}</h1>
     </header>
   );
 }
