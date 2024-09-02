ddd8c6e5e556cdc67c035b8a324b5d9905d35ec4
heejae <leeheejaelcode@gmail.com>
2024-09-03 00:07:03 +0900

    [수정] AppDivider 수정 - 디자인 수정

--- src/components/AppDivider.jsx
+++ src/components/AppDivider.jsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 
 function AppDivider() {
   return (
-    <hr className="my-8 border-t-[0.5px] border-solid border-t-accent w-1/4 max-w-md" />
+    <hr className="my-1 border-t-[0.5px] border-solid border-t-accent max-w-md" />
   );
 }
 
