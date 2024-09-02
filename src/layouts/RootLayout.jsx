f9f8f34afb654783cd201ee83304c6416579c34d
heejae <leeheejaelcode@gmail.com>
2024-09-03 00:09:27 +0900

    [수정] RootLayout 수정 - 기본 디자인 수정

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,7 +5,7 @@ import { AppSpinner } from '@/components';
 function RootLayout() {
   return (
     <div className="min-h-screen bg-black flex justify-center items-center flex-col text-white">
-      <main className="w-full relative pt-[44px] pb-[55px] box-border">
+      <main className="min-w-[320px] max-w-[320px] relative border border-solid border-white box-border">
         <Suspense fallback={<AppSpinner />}>
           <Outlet />
         </Suspense>
