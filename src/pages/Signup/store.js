dac7c7d86f3c04824833ad512e23a2b2aaa8d773
heejae <leeheejaelcode@gmail.com>
2024-09-03 17:43:34 +0900

    [수정] 회원가입 store 수정 - 데이터 구조 변경 - 회원가입 로직 추가

--- src/pages/Signup/store.js
+++ src/pages/Signup/store.js
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import { produce } from 'immer';
 import { getRandomMinMax } from '@/utils';
+import { createData } from '@/api/axios';
 
 export const useSignupStore = create((set) => {
   const INITIAL_STATE = {
@@ -11,32 +12,55 @@ export const useSignupStore = create((set) => {
       passwordConfirm: false,
     },
     user: {
-      username: '',
+      nickName: '',
       password: '',
       confirmPassword: '',
       phoneNumber: '',
       email: '',
+      gender: '',
+      age: '',
+      agreementTerms: {
+        over14: false,
+        terms: false,
+        privacy: false,
+        thirdParty: false,
+        thirdPartyOptional: false,
+        marketingOptional: false,
+        smsOptional: false,
+      },
     },
-    checkedItems: {
-      over14: false,
-      terms: false,
-      privacy: false,
-      thirdParty: false,
-      thirdPartyOptional: false,
-      marketingOptional: false,
-      smsOptional: false,
-    },
-    gender: '',
-    age: '',
     allChecked: false,
-    isShow: false,
+    isVerificationCodeButtonDisabled: false,
     isPhoneNumberButtonDisabled: true,
+    isSignupButtonDisabled: true,
+    verificationCode: null,
+    isVerificationCode: false,
+  };
+
+  const updateSignupButtonState = (draft) => {
+    const { user } = draft;
+    draft.isSignupButtonDisabled = !(
+      user.nickName &&
+      user.email &&
+      user.phoneNumber &&
+      user.gender &&
+      user.age &&
+      user.password &&
+      user.confirmPassword &&
+      draft.verificationCode &&
+      draft.isVerificationCode &&
+      user.agreementTerms.over14 &&
+      user.agreementTerms.terms &&
+      user.agreementTerms.privacy &&
+      user.agreementTerms.thirdParty
+    );
   };
 
   const setUserField = (field, value) =>
     set(
       produce((draft) => {
         draft.user[field] = value;
+        updateSignupButtonState(draft);
       })
     );
 
@@ -62,6 +86,15 @@ export const useSignupStore = create((set) => {
       })
     );
   };
+  const handleNickNameChange = (value) => {
+    const nickNameRegex = /^[가-힣a-zA-Z0-9]{1,8}$/;
+    if (nickNameRegex.test(value)) {
+      setUserField('nickName', value);
+    }
+    //  else {
+    //   alert('특수문자 제외 8글자까지만 작성해주세요');
+    // }
+  };
 
   const handleEmailChange = (value) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -86,26 +119,31 @@ export const useSignupStore = create((set) => {
   };
 
   const handlePhoneNumberCheck = () => {
-    const phoneNumberRegex = /^0\d{10}$/;
+    const random = getRandomMinMax(100000, 999999);
+    alert(`인증번호 : ${random}`);
     set(
       produce((draft) => {
-        if (
-          !draft.user.phoneNumber ||
-          !phoneNumberRegex.test(draft.user.phoneNumber)
-        ) {
-          alert('제대로된 전화번호를 입력해주세요');
-        } else {
-          const random = getRandomMinMax(100000, 999999);
-          alert(`인증번호 : ${random}`);
-          draft.isShow = true;
+        draft.verificationCode = random;
+        draft.isVerificationCodeButtonDisabled = true;
+        updateSignupButtonState(draft);
+      })
+    );
+  };
+
+  const handleVerificationCodeCheck = (value) => {
+    set(
+      produce((draft) => {
+        if (value === draft.verificationCode.toString()) {
+          draft.isVerificationCode = true;
+          updateSignupButtonState(draft);
         }
       })
     );
   };
 
   const handlePasswordCheck = (value) => {
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-
+    const passwordRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+[\]{};':"\\|,.<>?/-]{8,}$/;
     if (passwordRegex.test(value)) {
       setUserField('password', value);
       deactivateAuthMessage('passwordVerification');
@@ -118,39 +156,32 @@ export const useSignupStore = create((set) => {
     set(
       produce((draft) => {
         draft.user.confirmPassword = value;
-
         const { password, confirmPassword } = draft.user;
         if (password !== confirmPassword) {
           draft.authMessages.passwordConfirm = true;
         } else {
           draft.authMessages.passwordConfirm = false;
         }
+        updateSignupButtonState(draft);
       })
     );
   };
   const handleGenderCheck = ({ value }) => {
-    set(
-      produce((draft) => {
-        draft.gender = value;
-      })
-    );
+    setUserField('gender', value);
   };
 
   const handleAgeCheck = ({ value }) => {
-    set(
-      produce((draft) => {
-        draft.age = value;
-      })
-    );
+    setUserField('age', value);
   };
 
   const handleAllCheck = () => {
     set(
       produce((draft) => {
         draft.allChecked = !draft.allChecked;
-        Object.keys(draft.checkedItems).forEach(
-          (key) => (draft.checkedItems[key] = draft.allChecked)
+        Object.keys(draft.user.agreementTerms).forEach(
+          (key) => (draft.user.agreementTerms[key] = draft.allChecked)
         );
+        updateSignupButtonState(draft);
       })
     );
   };
@@ -159,24 +190,51 @@ export const useSignupStore = create((set) => {
     const { name } = target;
     set(
       produce((draft) => {
-        draft.checkedItems[name] = !draft.checkedItems[name];
-        draft.allChecked = Object.values(draft.checkedItems).every(Boolean);
+        draft.user.agreementTerms[name] = !draft.user.agreementTerms[name];
+        draft.allChecked = Object.values(draft.user.agreementTerms).every(
+          Boolean
+        );
+        updateSignupButtonState(draft);
       })
     );
+  };
+
+  const handleSignupButtonClick = async () => {
+    const user = useSignupStore.getState().user;
+
+    const userData = {
+      nickName: user.nickName,
+      password: user.password,
+      passwordConfirm: user.confirmPassword,
+      phoneNumber: user.phoneNumber,
+      email: user.email,
+      gender: user.gender,
+      age: user.age,
+      agreementTerms: JSON.stringify(user.agreementTerms),
+    };
+    try {
+      const data = await createData('users', userData);
+      console.log('response', data);
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
   };
 
   return {
     ...INITIAL_STATE,
     handleMethod: {
+      handleNickNameChange,
       handleEmailChange,
       handlePhoneNumberChange,
       handlePhoneNumberCheck,
+      handleVerificationCodeCheck,
       handlePasswordCheck,
       handlePasswordConfirmCheck,
       handleAllCheck,
       handleCheckboxChange,
       handleGenderCheck,
       handleAgeCheck,
+      handleSignupButtonClick,
     },
     authMethod: {
       activateAuthMessage,
