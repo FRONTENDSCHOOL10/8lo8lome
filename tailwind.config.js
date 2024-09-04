dd721f32d93dddb042c116c1bfacd226f8c12517
heejae <leeheejaelcode@gmail.com>
2024-09-05 00:48:00 +0900

    [설정] tailwind 색상 변수 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,22 +9,9 @@ export default {
         white: '#ffffff',
         subBg: '#2A2A2A',
         warning: '#fd0000',
+        borderPrimary: '#16efa4',
       },
     },
   },
-
-  // 참고: https://tailwindcss.com/docs/plugins
-  plugins: [
-    //   ({ addComponents, addUtilities }) => {
-    //     addComponents({
-    //       '.euid-button': {
-    //         '@apply border-2 border-solid border-indigo-600 py-2 px-5 rounded-lg text-indigo-600 font-medium uppercase':
-    //           {},
-    //       },
-    //     });
-    //     addUtilities({
-    //       '.a11y-hidden': {},
-    //     });
-    //   },
-  ],
+  plugins: [],
 };
