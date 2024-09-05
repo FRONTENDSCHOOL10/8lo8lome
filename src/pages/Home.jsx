5e2eec018d461dd389f380ba10fb39cb87f27628
heejae <leeheejaelcode@gmail.com>
2024-09-05 12:03:47 +0900

    [수정] Home.jsx alt 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <section className="min-h-[600px] bg-[url('../assets/bg.avif')] bg-no-repeat bg-cover bg-right-top flex flex-col justify-evenly gap-20 pt-s98 pb-s50">
       <h1 className="mx-auto">
-        <img width={200} height={125} src="../assets/logo.svg" alt="로고" />
+        <img width={200} height={125} src="../assets/logo.svg" alt="다있짐" />
       </h1>
       <div className="flex flex-col justify-center items-center gap-[10px] px-s12">
         <Link to="/signup" className={LinkClass + `bg-mainColor text-black`}>
