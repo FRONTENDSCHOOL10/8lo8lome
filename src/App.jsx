f7b453f6346c1e267b510ae679830f33fa4bba34
daggi-01 <dhemd1111@gmail.com>
2024-09-05 16:40:10 +0900

    [추가/ 삭제] -App파이 코드 삭제 수정 -sprite파일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,11 @@ import { useState, useEffect } from 'react';
 import router from '@/router';
 import { RouterProvider } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import AppLoading from './components/AppLoading';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, []);
   return (
     <HelmetProvider>
-      {loading ? <AppLoading /> : <RouterProvider router={router} />}
+      <RouterProvider router={router} />
     </HelmetProvider>
   );
 }
