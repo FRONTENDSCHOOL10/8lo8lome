0d615026caba7d24d02bed566494a3bd95f75d73
daggi-01 <dhemd1111@gmail.com>
2024-09-06 01:23:23 +0900

    [수정] -tailwind 변수 수정 -MyCoupon, MyPage 수정

--- src/pages/MyPage/MyCoupon.jsx
+++ src/pages/MyPage/MyCoupon.jsx
@@ -10,20 +10,20 @@ const coupon = [
 
 export default function MyCoupon() {
   return (
-    <section className="p-s20 size-80">
-      <h2 className="mb-4 text-f18 text-white">내 회원권 정보</h2>
-      <div className="flex flex-row items-start w-full h-156px">
-        <ul className="w-full h-40 p-4 rounded shadow-md bg-gradient-to-br from-mainColor to-green-900">
+    <section className="p-s20 size-80" aria-label="내 회원권 쿠폰">
+      <h2 className="text-white mb-s16 text-f18">내 회원권 정보</h2>
+      <div className="flex flex-row items-start w-full h-h$156">
+        <ul className="w-full p-4 rounded shadow-md bg-gradient-to-br from-mainColor to-green-900 w-">
           {coupon.map((item) => {
             return (
               <li
                 key={item.id}
                 className="flex flex-col text-base text-black mb-1.5"
               >
-                <p className="font-semibold">{item.subTitle1}</p>
-                <p className="text-2xl font-bold">{item.title}</p>
+                <p className="font-semibold text-f14">{item.subTitle1}</p>
+                <p className="font-bold text-f16">{item.title}</p>
                 <p className="mb-5 text-f14">{item.subTitle2}</p>
-                <p className="text-base font-bold">{item.dateInfo}</p>
+                <p className="font-bold text-f16">{item.dateInfo}</p>
               </li>
             );
           })}
