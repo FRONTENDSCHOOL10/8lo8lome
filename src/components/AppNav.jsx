0de535c118a19cc0a7771d79d768799ae8a8d9e1
heejae <leeheejaelcode@gmail.com>
2024-09-07 01:46:58 +0900

    [스타일링] AppNav 포지션 고정

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -6,7 +6,7 @@ function AppNav() {
   const [items] = useState(navigationItems);
 
   return (
-    <nav className="bg-mainBg">
+    <nav className="bg-mainBg fixed bottom-[129px] w-[38%] border border-solid border-white">
       <h2 className="sr-only">페이지 탐색</h2>
       {items.length > 0 && (
         <ul className="py-s16 w-4/5 max-w-6xl mx-auto flex justify-center gap-4">
