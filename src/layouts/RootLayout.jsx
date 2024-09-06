60cd4659f5a21c0a39b3ddf75f0ab83cab6b3b1d
heejae <leeheejaelcode@gmail.com>
2024-09-06 15:33:11 +0900

    [스타일링] min-h 설정

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -19,7 +19,8 @@ function RootLayout() {
 
   return (
     <div className="min-h-screen bg-mainBg flex justify-center items-center flex-col text-white">
-      <main className="min-w-[320px] max-w-[320px] relative border border-solid border-white box-border">
+      <main className="min-w-[340px] min-h-[640px] relative border border-solid border-white box-border">
+        {/* 반응형처럼 보이게 하려면 w-full 추가해야할듯.. */}
         <Suspense fallback={<AppSpinner />}>
           <Outlet />
         </Suspense>
