743dc078a8fec930cbef410dffbca30f5eaf4921
daggi-01 <dhemd1111@gmail.com>
2024-09-07 02:45:26 +0900

    [기능] -pocketbase userId받아오는 코드 작성

--- src/stores/myProfileStore.js
+++ src/stores/myProfileStore.js
@@ -1,5 +1,23 @@
 import { create } from 'zustand';
 import { produce } from 'immer';
 import { createData, getData } from '@/api/CRUD';
+import pb from '@/api/pb';
+import { error } from 'console';
 
-const 
+ const getLgonInUserId = async () => {
+  try{
+    const userId = pb.authStore.model?.id
+
+    if(!userId){
+      throw new Error(error.message)
+    }
+  }
+  const userData = awiat getData('users', userId);
+
+  const filterData = {
+    id: userData.id,
+    email:userData.email,
+    nickname: userData.nickname,
+    profile:userData.prpfile ? `${pb.}`
+  }
+ }
