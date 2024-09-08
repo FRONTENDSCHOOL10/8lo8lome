17ec75c4ec8ae3389cf18233216d230c1d04b031
daggi-01 <dhemd1111@gmail.com>
2024-09-08 14:51:13 +0900

    [수정] -myProfileStore.js 코드 수정

--- src/stores/myProfileStore.js
+++ src/stores/myProfileStore.js
@@ -1,23 +1,7 @@
+import { useEffect, useState } from 'react';
 import { create } from 'zustand';
 import { produce } from 'immer';
-import { createData, getData } from '@/api/CRUD';
+// import { createData, getData } from '@/api/CRUD';
+import { Link, useLocation } from 'react-router-dom';
 import pb from '@/api/pb';
-import { error } from 'console';
-
- const getLgonInUserId = async () => {
-  try{
-    const userId = pb.authStore.model?.id
-
-    if(!userId){
-      throw new Error(error.message)
-    }
-  }
-  const userData = awiat getData('users', userId);
-
-  const filterData = {
-    id: userData.id,
-    email:userData.email,
-    nickname: userData.nickname,
-    profile:userData.prpfile ? `${pb.}`
-  }
- }
+ 
