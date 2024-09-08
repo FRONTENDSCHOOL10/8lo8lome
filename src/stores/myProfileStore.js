be85cdf1e24ad645e204d78679fd3c487c06e0ee
daggi-01 <dhemd1111@gmail.com>
2024-09-09 07:22:25 +0900

    [삭제] -MyProfileStore 삭제

--- src/stores/myProfileStore.js
+++
@@ -1,7 +0,0 @@
-import { useEffect, useState } from 'react';
-import { create } from 'zustand';
-import { produce } from 'immer';
-// import { createData, getData } from '@/api/CRUD';
-import { Link, useLocation } from 'react-router-dom';
-import pb from '@/api/pb';
- 
