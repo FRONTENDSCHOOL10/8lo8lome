cf9765fd7699ada7ef1dd27e3132e6d4d3f14a56
2yerin <lyerin0113@naver.com>
2024-09-09 16:51:09 +0900

    [기능/수정] 지도 렌더링, 불필요한 코드 삭제, 코드리뷰 반영
    - map.jsx에서 카카오 지도 api 사용
    - gymlist에서 헬스장 정보 리스트 a태그에서 Link로 변경
    - 쓰로틀 불필요한 주석 코드 삭제

--- src/pages/Main/GymList.jsx
+++ src/pages/Main/GymList.jsx
@@ -2,6 +2,7 @@ import { AppCheckboxInput } from '@/components';
 import { memo } from 'react';
 import { useSearchStore } from '@/stores/mainStore';
 import getPbImageURL from '@/utils/getPbImageURL';
+import { Link } from 'react-router-dom';
 
 function GymList() {
   const filterGyms = useSearchStore((state) => state.filterGyms);
@@ -12,46 +13,52 @@ function GymList() {
         const imgUrl = getPbImageURL(item);
         return (
           <li key={item.id}>
-            <a
-              href=""
-              className="text-white flex gap-[0.625rem]
+            {/* Link 이동은 아직 안 넣음 */}
+            <Link to={'/filter'} aria-label="헬스장 상세 정보 페이지">
+              <div
+                className="text-white flex gap-[0.625rem]
             bg-subBg rounded p-[0.625rem]"
-            >
-              <img src={imgUrl[0]} alt="헬스장 사진" className="max-w-[90px]" />
-              <div className="flex flex-col w-full">
-                <div className="flex justify-between items-center">
-                  <h2 className="text-base">{item.name}</h2>
-                  <AppCheckboxInput
-                    label={'헬스장 정보 찜하기'}
-                    isHiddenLabel
-                    // name="over14"
-                    // isChecked={over14}
-                    // onChange={handleCheckboxChange}
-                    unCheckedSvgId="heart-unclick"
-                    checkedSvgId="heart-click"
-                    checkedColor="text-red-500"
-                  />
-                </div>
+              >
+                <img
+                  src={imgUrl[0]}
+                  alt="헬스장 사진"
+                  className="max-w-[90px]"
+                />
+                <div className="flex flex-col w-full">
+                  <div className="flex justify-between items-center">
+                    <h2 className="text-base">{item.name}</h2>
+                    <AppCheckboxInput
+                      label={'헬스장 정보 찜하기'}
+                      isHiddenLabel
+                      // name="over14"
+                      // isChecked={over14}
+                      // onChange={handleCheckboxChange}
+                      unCheckedSvgId="heart-unclick"
+                      checkedSvgId="heart-click"
+                      checkedColor="text-red-500"
+                    />
+                  </div>
 
-                <p className="text-f12">
-                  가격 : {item.oneDayPrice.toLocaleString()}원
-                </p>
-                <div className="flex justify-between text-[0.625rem] pt-4">
-                  <p>{item.address}km</p>
-                  <div className="flex items-center gap-1">
-                    <svg
-                      role="icon"
-                      aria-label="별점"
-                      className="w-3 h-3 fill-yellow-300"
-                    >
-                      <use href="/assets/sprite.svg#star" />
-                    </svg>
+                  <p className="text-f12">
+                    가격 : {item.oneDayPrice.toLocaleString()}원
+                  </p>
+                  <div className="flex justify-between text-[0.625rem] pt-4">
+                    <p>{item.address}km</p>
+                    <div className="flex items-center gap-1">
+                      <svg
+                        role="icon"
+                        aria-label="별점"
+                        className="w-3 h-3 fill-yellow-300"
+                      >
+                        <use href="/assets/sprite.svg#star" />
+                      </svg>
 
-                    <p>{item.rating}</p>
+                      <p>{item.rating}</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </a>
+            </Link>
           </li>
         );
       })}
