7f751f51876d95964cc76f6437ca19d1d42319a6
heejae <leeheejaelcode@gmail.com>
2024-09-09 14:00:17 +0900

    [수정] getPbImageURL 코드 수정 및 적용
    - GymList
    - MyProfile

--- src/utils/getPbImageURL.js
+++ src/utils/getPbImageURL.js
@@ -1,5 +1,16 @@
-const getPbImageURL = (id, profile) => {
-  return `${import.meta.env.VITE_PB}/api/files/users/${id}/${profile}`;
+const getPbImageURL = (item) => {
+  const { id, photo, collectionId } = item;
+
+  // review가 true로 변환되고, photo 배열의 길이가 2 이상일 경우 URL 배열 반환
+  if (Array.isArray(photo) && photo.length >= 2) {
+    return photo.map(
+      (photoItem) =>
+        `${import.meta.env.VITE_PB}/api/files/${collectionId}/${id}/${photoItem}`
+    );
+  }
+
+  // 기본 URL 반환
+  return `${import.meta.env.VITE_PB}/api/files/${collectionId}/${id}/${photo}`;
 };
 
 export default getPbImageURL;
