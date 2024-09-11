6ebfa671666cfa3d525921c72d8a3ad1f1f3e32a
heejae <leeheejaelcode@gmail.com>
2024-09-11 15:42:37 +0900

    [수정] chatMessages 리팩토링

--- src/pages/Chat/ChatMessages.jsx
+++ src/pages/Chat/ChatMessages.jsx
@@ -6,22 +6,16 @@ import { useParams } from 'react-router-dom';
 function ChatMessages() {
   const { roomId } = useParams();
   const messagesEndRef = useRef(null);
-  const { currentRoomMessages, userId, getChatMessages, updateChatRoom } =
-    useChatStore((s) => ({
+  const { currentRoomMessages, userId, getChatMessages } = useChatStore(
+    (s) => ({
       currentRoomMessages: s.currentRoomMessages[roomId] || [], // 기본값 설정
       userId: s.userId,
       getChatMessages: s.getChatMessages,
       updateChatRoom: s.updateChatRoom,
-    }));
+    })
+  );
 
   useEffect(() => {
-    if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, [currentRoomMessages]);
-
-  useEffect(() => {
-    // 데이터 가져오기 함수
     const fetchData = async () => {
       try {
         await getChatMessages(roomId);
@@ -29,27 +23,24 @@ function ChatMessages() {
         console.error('Error fetching data:', error);
       }
     };
+
     fetchData();
-    // 메시지와 채팅방 구독 설정
-    pb.collection('messages').subscribe('*', async (msg) => {
-      await fetchData(); // 채팅방 목록 업데이트
 
-      // 메시지가 새로 도착하면 lastMessage 업데이트
-      if (msg && msg.collectionName === 'messages') {
-        const roomId = msg.record.roomId; // 메시지에 포함된 roomId 가져오기
-        const lastMessage = msg.record.content; // 메시지 내용 가져오기
-
-        // 해당 채팅방의 lastMessage 업데이트
-        await updateChatRoom(roomId, {
-          lastMessage: lastMessage,
-        });
-      }
+    // 메시지 구독 설정
+    pb.collection('messages').subscribe('*', async () => {
+      await fetchData(); // 메시지가 업데이트되면 데이터 다시 가져오기
     });
+
+    // 스크롤을 메시지 목록 끝으로 이동
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+
     // 컴포넌트 언마운트 시 구독 해제
     return () => {
-      pb.collection('messages').unsubscribe();
+      pb.collection('messages').unsubscribe('*'); // 메시지 구독 해제
     };
-  }, [roomId, getChatMessages, updateChatRoom]);
+  }, [roomId, getChatMessages, currentRoomMessages]);
 
   return (
     <main className="overflow-y-auto p-4 bg-mainBg min-h-[468px] mt-[100px] mb-[60px]">
