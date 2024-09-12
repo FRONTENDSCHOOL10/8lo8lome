e61ee19fd0c23ed5edecf68d35ed2e52706271a0
heejae <leeheejaelcode@gmail.com>
2024-09-12 16:07:05 +0900

    [수정] AppLoading 수정

--- src/components/AppLoading.jsx
+++ src/components/AppLoading.jsx
@@ -1,43 +1,46 @@
 import { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
-import AppSpinner from './AppSpinner';
+import AppSpinner from '@/components/AppSpinner';
+import { bool } from 'prop-types';
 
-const alarm = [
-  {
-    id: 1,
-    notification: '로딩 중입니다.',
-  },
-  {
-    id: 2,
-    notification: '로딩이 끝났습니다.',
-  },
-];
+AppLoading.propTypes = {
+  isLoading: bool,
+};
+export default function AppLoading({ isLoading }) {
+  const [message, setMessage] = useState('로딩 중입니다 잠시만 기다려주세요.');
 
-export default function AppLoading() {
-  const [loading, setLoading] = useState(true);
-  const [message, setMessage] = useState(alarm[0].notification);
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-      setMessage(alarm[1].notification);
-    }, 3000); //3초후 로딩 종료
-    return () => clearTimeout(timer);
-  }, []);
-
-  // const loadingStart = document.querySelector('#loading-start');
-  // const loadingEnd = document.querySelector('#loading-end');
+    if (isLoading) {
+      setMessage('로딩 중입니다 잠시만 기다려주세요.');
+    } else {
+      setMessage('로딩이 완료되었습니다.');
+    }
+  }, [isLoading]);
 
   return (
     <>
-      {loading &&
+      {isLoading &&
         createPortal(
-          <div className="fixed inset-y-0 z-50 flex items-center justify-center w-full h-full bg-white bg-opacity-50">
-            <AppSpinner size={50} duration={1.5} />
-            <p>{message}</p>
+          <div
+            className="fixed inset-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-30 "
+            aria-live="assertive"
+            role="alert"
+          >
+            <div className="text-center">
+              <AppSpinner size={50} duration={1.5} />
+              <p className="sr-only">{message}</p>
+            </div>
           </div>,
-          document.body
+          document.querySelector('#loading-start') || document.body
         )}
-      {!loading && <p>{message}</p>}
+
+      {!isLoading &&
+        createPortal(
+          <div>
+            <p className="sr-only">{message}</p>
+          </div>,
+          document.querySelector('#loading-end') || document.body
+        )}
     </>
   );
 }
