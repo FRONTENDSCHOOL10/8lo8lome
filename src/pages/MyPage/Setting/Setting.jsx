3080b041ff2b55714533208df7a250e121e66604
heejae <leeheejaelcode@gmail.com>
2024-09-12 17:49:51 +0900

    [기능] 회원탈퇴 기능 구현

--- src/pages/MyPage/Setting/Setting.jsx
+++ src/pages/MyPage/Setting/Setting.jsx
@@ -1,21 +1,34 @@
-import { AppHeader } from '@/components';
+import { AppHeader, AppStatusPage } from '@/components';
 import AppMeta from '@/components/AppMeta';
 import { Link } from 'react-router-dom';
 import { memo } from 'react';
+import { useDeleteIdStore } from '@/stores/deleteIdStore';
 
 function Setting() {
+  const { handleDeleteUser, isDeleteId } = useDeleteIdStore((s) => ({
+    handleDeleteUser: s.handleDeleteUser,
+    isDeleteId: s.isDeleteId,
+  }));
+
+  if (isDeleteId) {
+    return <AppStatusPage status="deleteId" />;
+  }
+
   return (
     <>
       <AppMeta title="설정" description="설정 페이지 입니다." />
       <AppHeader>설정</AppHeader>
-
       <section
         className="flex items-center justify-center w-340px px-s20 mt-[100px]"
         aria-label="설정목록"
       >
         <ul className="flex flex-col items-start justify-center">
           <li className="flex items-center border-b border-solid py-s20 w-s278 h-s62 border-strokeBlack">
-            <Link to="/SignOut" className="flex items-center ">
+            <button
+              to="/SignOut"
+              className="flex items-center"
+              onClick={handleDeleteUser}
+            >
               <svg
                 role="icon"
                 aria-label="탈퇴"
@@ -26,7 +39,6 @@ function Setting() {
               <p className="font-semibold text-white text-f16 mr-s10">
                 탈퇴하기
               </p>
-
               <svg
                 role="icon"
                 aria-label="탈퇴 하기로 이동하는 버튼"
@@ -34,7 +46,7 @@ function Setting() {
               >
                 <use href="/assets/sprite.svg#arrow-forward" />
               </svg>
-            </Link>
+            </button>
           </li>
           <li className="flex items-center border-b border-solid py-s20 w-s278 h-s62 border-strokeBlack">
             <Link to="/FindPassword" className="flex items-center ">
@@ -48,7 +60,6 @@ function Setting() {
               <p className="font-semibold text-white text-f16 mr-s10">
                 비밀번호 변경
               </p>
-
               <svg
                 role="icon"
                 aria-label="비밀번호 변경 페이지 이동 버튼"
