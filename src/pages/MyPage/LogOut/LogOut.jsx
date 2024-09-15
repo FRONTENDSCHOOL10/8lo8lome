924fa8899741853cc3d735f30ba4f1a4a2d16255
daggi-01 <dhemd1111@gmail.com>
2024-09-16 00:21:51 +0900

    [삭제] -LogOut 패이지 삭제

--- src/pages/MyPage/LogOut/LogOut.jsx
+++
@@ -1,8 +0,0 @@
-// import AppStatusPage from '@/components/AppStatusPage';
-// import { memo } from 'react';
-
-// function LogOut() {
-//   return <AppStatusPage status="logout" />;
-// }
-
-// export default memo(LogOut);
