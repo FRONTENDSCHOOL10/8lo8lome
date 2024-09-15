ccca379bd9128edd87b23a57c716789558bb93d6
daggi-01 <dhemd1111@gmail.com>
2024-09-16 00:21:24 +0900

    [추가] -LogOut기능 추가

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -4,8 +4,20 @@ import MyCoupon from './MyCoupon';
 import MyProfile from './MyProfile';
 import MyInfo from './MyInfo';
 import AppMeta from '@/components/AppMeta';
+import { useLogoutStore } from '@/stores/logOutStore';
+import { AppStatusPage } from '@/components';
+import { memo } from 'react';
 
-export default function MyPage() {
+function MyPage() {
+  const { handleLogout, isLoggedOut } = useLogoutStore((s) => ({
+    handleLogout: s.handleLogout,
+    isLoggedOut: s.isLoggedOut,
+  }));
+
+  if (isLoggedOut) {
+    return <AppStatusPage status="logout" />;
+  }
+
   return (
     <>
       <AppMeta title="마이페이지" description="마이페이지 페이지입니다" />
@@ -14,6 +26,29 @@ export default function MyPage() {
       <MyCoupon />
       <MyInfo />
       <AppNav />
+
+      <ul className="w-full px-s20 mb-s50">
+        <li className="flex items-center border-b border-solid h-s62 border-strokeBlack">
+          <button className="flex items-center w-full" onClick={handleLogout}>
+            <svg
+              role="icon"
+              aria-label="로그아웃"
+              className="text-white w-s18 h-s18 mr-s10 ml-s10"
+            >
+              <use href="/assets/sprite.svg#log-out" />
+            </svg>
+            <p>로그아웃</p>
+            <svg
+              role="icon"
+              aria-label="로그아웃 버튼"
+              className="text-white w-s18 h-s18 mr-s10"
+            >
+              <use href="/assets/sprite.svg#arrow-forward" />
+            </svg>
+          </button>
+        </li>
+      </ul>
     </>
   );
 }
+export default memo(MyPage);
