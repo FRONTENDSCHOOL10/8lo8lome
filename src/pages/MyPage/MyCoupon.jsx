c98ab9e03109c1f1b05c7ccb6cd72c6d2f19a20c
daggi-01 <dhemd1111@gmail.com>
2024-09-19 16:20:49 +0900

    [수정] -MyCoupon 접근성 관련으로 수정, 키보드접근 가능 하나 보더가 안뜸

--- src/pages/MyPage/MyCoupon.jsx
+++ src/pages/MyPage/MyCoupon.jsx
@@ -1,8 +1,8 @@
-import { Pagination } from 'swiper/modules';
+import { Pagination, Keyboard, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/pagination';
 import { memo } from 'react';
+
 const coupon = [
   {
     id: 1,
@@ -43,16 +43,27 @@ const coupon = [
 
 function MyCoupon() {
   return (
-    <section className="p-s20" aria-label="내 회원권 쿠폰">
+    <section className="p-s20" aria-label="내 회원권 쿠폰" role="region">
       <h2 className="text-white mb-s16 text-f18">내 회원권 정보</h2>
       <Swiper
-        modules={[Pagination]}
+        modules={[Pagination, Keyboard, A11y]}
         spaceBetween={16}
         slidesPerView={1.1}
-        pagination={{ clickable: true }}
+        pagination={{
+          clickable: true,
+          el: '.pager',
+          enabled: true,
+          containerMessage: '다음 쿠폰',
+        }}
+        keyboard={{ enavled: true, pageUpDown: true }}
+        a11y={{
+          paginationBulletMessage: '슬라이드 {{index}}',
+        }}
+        aria-live="polite"
+        aria-label="쿠폰 슬라이더"
       >
         {coupon.map((item) => (
-          <SwiperSlide key={item.id}>
+          <SwiperSlide key={item.id} aria-labelledby={`slide-${item.id}`}>
             <ul className="flex flex-row rounded-md shadow-md p-s20 bg-gradient-to-br from-mainColor to-green-900 h-s156">
               <li className="flex flex-col text-base text-black ">
                 <p className="font-semibold text-f14 mb-s6">{item.subTitle1}</p>
@@ -63,6 +74,7 @@ function MyCoupon() {
             </ul>
           </SwiperSlide>
         ))}
+        <div className="pager" aria-label="페이지 네비게이션"></div>
       </Swiper>
     </section>
   );
