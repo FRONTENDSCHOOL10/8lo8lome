1deb511bce36ac73bf3cbf06e21e0e7deeda78fc
heejae <leeheejaelcode@gmail.com>
2024-09-19 14:44:46 +0900

    [수정] mapStore 불필요한 코드 삭제

--- src/stores/mapStore.js
+++ src/stores/mapStore.js
@@ -11,9 +11,9 @@ export const useMapStore = create((set) => {
   // 포켓베이스에서 헬스장 정보를 가져오는 함수
   const fetchGyms = async () => {
     try {
-      const { filterGyms } = mainStore.getState().searchInput;
+      const { filteredGymsByDistance } = mainStore.getState().searchInput;
       const gymsWithCoords = await Promise.all(
-        filterGyms.map(async (gym) => {
+        filteredGymsByDistance.map(async (gym) => {
           try {
             // 주소를 위도와 경도로 변환
             const { latitude, longitude } = await geocodeAddress(gym.address);
@@ -25,9 +25,6 @@ export const useMapStore = create((set) => {
               photo: gym.photo,
               latitude,
               longitude,
-              photo: gym.photo, // 첫 번째 이미지만 사용
-              latitude, // 변환된 위도
-              longitude, // 변환된 경도
               collectionId: gym.collectionId,
               rating: gym.rating,
             };
@@ -41,8 +38,6 @@ export const useMapStore = create((set) => {
               photo: gym.photo,
               latitude: null,
               longitude: null,
-              latitude: null, // 변환 실패 시 null로 설정
-              longitude: null, // 변환 실패 시 null로 설정
               collectionId: gym.collectionId,
               rating: gym.rating,
             };
