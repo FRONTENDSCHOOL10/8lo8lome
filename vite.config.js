e1c5b8ad5c64e5de549c49010921094ac0436234
daggi-01 <dhemd1111@gmail.com>
2024-09-20 13:54:10 +0900

    [환경설정] -vite.config.js 에 롤업옵션 추가

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,32 @@ const viteConfig = defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          'react-router-dom': ['react-router-dom'],
+          'react-ecosystem': [
+            'immer',
+            'use-immer',
+            'motion',
+            'react-helmet-async',
+            'react-hot-toast',
+            'react-swipeable',
+            'zustand',
+            'swiper',
+            'axios',
+          ],
+          components: [
+            './src/components/AppList.jsx',
+            './src/pages/Main/GymList.jsx',
+            './src/components/AppNav.jsx',
+          ],
+        },
+      },
+    },
+  },
 });
 
 export default viteConfig;
