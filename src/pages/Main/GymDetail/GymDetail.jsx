762f3985055a051f4974a36a0908ea8f3286d9d2
heejae <leeheejaelcode@gmail.com>
2024-09-20 15:00:57 +0900

    [수정] GymDetail 접근성 강화

--- src/pages/Main/GymDetail/GymDetail.jsx
+++ src/pages/Main/GymDetail/GymDetail.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
-import { AppHeader } from '@/components';
-import { memo, useEffect } from 'react';
+import { AppHeader, AppLoading } from '@/components';
+import { memo, useEffect, useState } from 'react';
 import { mainStore } from '@/stores/mainStore';
 import GymDetailHeader from './GymDetailHeader';
 import PriceList from './PriceList';
@@ -9,44 +9,52 @@ import LocationMap from './LocationMap';
 import TrainerList from './TrainerList';
 import Refundpolicy from './Refundpolicy';
 import GymDetailFooter from './GymDetailFooter';
-import { AppSpinner } from '@/components';
 
 function GymDetail() {
   const { gymId } = useParams();
-
+  const [isLoading, setIsLoading] = useState(true); // 로딩 상태
   const { fetchGymDetails, gymData } = mainStore((s) => ({
     fetchGymDetails: s.handleMethod.fetchGymDetails,
     gymData: s.searchInput.gymData,
   }));
 
   useEffect(() => {
-    if (gymId) {
-      fetchGymDetails(gymId);
-    }
-  }, [gymId, fetchGymDetails]);
+    const loadGymDetails = async () => {
+      if (gymId) {
+        try {
+          await fetchGymDetails(gymId);
+        } catch (error) {
+          console.error('Error fetching gym details:', error);
+          setIsLoading(false);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    };
 
-  if (!gymData || gymData.id !== gymId) {
-    return (
-      <div className="w-full h-s386 flex justify-center items-center">
-        <AppSpinner />
-      </div>
-    );
-  }
+    loadGymDetails();
+  }, [gymId, fetchGymDetails]);
 
   return (
     <>
-      <AppHeader>{gymData.name}</AppHeader>
-
+      <AppHeader>{gymData?.name}</AppHeader>
       <div className="flex flex-col gap-s20">
-        <GymDetailHeader />
-        <PriceList />
-        <AmenitiesList />
-        <LocationMap />
-        <TrainerList />
-        <Refundpolicy />
-        <GymDetailFooter />
+        {isLoading ? (
+          <AppLoading isLoading={isLoading} />
+        ) : (
+          <>
+            <GymDetailHeader />
+            <PriceList />
+            <AmenitiesList />
+            <LocationMap />
+            <TrainerList />
+            <Refundpolicy />
+            <GymDetailFooter />
+          </>
+        )}
       </div>
     </>
   );
 }
+
 export default memo(GymDetail);
