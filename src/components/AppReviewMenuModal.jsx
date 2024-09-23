5c53ceaa8ef7cb023422dc9552b93b3c16b62bf6
2yerin <lyerin0113@naver.com>
2024-09-23 17:51:58 +0900

    [수정] 리뷰 메뉴 모달창 수정

--- src/components/AppReviewMenuModal.jsx
+++ src/components/AppReviewMenuModal.jsx
@@ -2,20 +2,18 @@ import { memo } from 'react';
 
 function AppReviewMenuModal({ onClose }) {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <ul className="bg-white p-4 rounded shadow-lg">
-        <li>
-          <button className="text-blue-500" onClick={onClose}>
-            수정하기
-          </button>
-        </li>
-        <li>
-          <button className="text-red-500" onClick={onClose}>
-            삭제하기
-          </button>
-        </li>
-      </ul>
-    </div>
+    <ul>
+      <li>
+        <button className="text-blue-500" onClick={onClose}>
+          수정하기
+        </button>
+      </li>
+      <li>
+        <button className="text-red-500" onClick={onClose}>
+          삭제하기
+        </button>
+      </li>
+    </ul>
   );
 }
 
