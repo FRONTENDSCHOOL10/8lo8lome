8f6b57fc4d3b21080bc8c64983aac03eb89d1b8a
daggi-01 <dhemd1111@gmail.com>
2024-09-23 17:46:25 +0900

    [주석] -PaymentList 리뷰보기 주석처리 [삭제] -유효성 검사 코드 삭제

--- src/stores/myPageStore.js
+++ src/stores/myPageStore.js
@@ -3,7 +3,6 @@ import { produce } from 'immer';
 import pb from '@/api/pb';
 import { getFirstListItem, updateData } from '@/api/CRUD';
 import { getPbImageURL } from '@/utils';
-import { NICKNAME_REG, EMAIL_REG } from '@/constant';
 
 export const useMyPageStore = create((set) => {
   const INITIAL_s = {
@@ -11,13 +10,17 @@ export const useMyPageStore = create((set) => {
       id: pb.authStore.model?.id || '',
       nickName: pb.authStore.model?.nickName || '',
       email: pb.authStore.model?.email || '',
+      phoneNumber: pb.authStore.model?.phoneNumber || '',
       profileImage: '',
       collectionName: 'users',
     },
     isLogin: pb.authStore.isValid,
     isNicknameDisabled: true,
     isNickname: null,
-    isEmailValid: null, // 이메일 유효성 상태
+    isEmailDisabled: null,
+    isEmail: null,
+    isPhoneNumberDisabled: null,
+    isPhoneNumber: null,
   };
 
   // 상태를 설정하는 함수
@@ -30,16 +33,6 @@ export const useMyPageStore = create((set) => {
   };
 
   const checkNicknameDuplicate = async (nickName) => {
-    // 유효한 닉네임인지 정규 표현식으로 검사
-    if (!NICKNAME_REG.test(nickName)) {
-      set(
-        produce((s) => {
-          s.isNickname = null; // 유효하지 않은 닉네임
-        })
-      );
-      return false;
-    }
-
     try {
       // 닉네임에 해당하는 첫 번째 사용자 항목을 가져옵니다.
       const result = await getFirstListItem('users', 'nickName', nickName);
@@ -71,6 +64,7 @@ export const useMyPageStore = create((set) => {
         id: authData.id,
         nickname: authData.nickName,
         email: authData.email,
+        phoneNumber: authData.phoneNumber,
         profileImage: getPbImageURL(authData),
       };
       set(
@@ -81,48 +75,58 @@ export const useMyPageStore = create((set) => {
     }
   };
 
-  const checkEmailDuplicate = async () => {
-    const userEmail = useMyPageStore.getState().userData.email;
-
-    // 이메일 유효성 검사
-    if (!EMAIL_REG.test(userEmail)) {
-      set(
-        produce((draft) => {
-          draft.isEmailValid = '유효하지 않은 이메일 형식입니다.'; // 유효하지 않은 이메일 메시지
-        })
-      );
-      return false;
-    }
-
+  const checkEmailDuplicate = async (email) => {
     try {
-      // 이메일 중복 확인
-      const emailResult = await getFirstListItem('users', 'email', userEmail);
-
-      // 상태 업데이트
+      const emailRsult = await getFirstListItem('users', 'email', email);
       set(
-        produce((draft) => {
-          draft.isEmailValid = emailResult;
-
-          console.log('getFirstListItem result:', emailResult); // 반환된 이메일 결과 확인
+        produce((s) => {
+          s.isEmail = !emailRsult;
         })
       );
 
-      return !emailResult; // 이메일이 존재하면 true, 존재하지 않으면 false 반환
+      return !emailRsult;
     } catch (error) {
       console.error('이메일 중복 확인 실패:', error);
-
       set(
-        produce((draft) => {
-          draft.isEmailValid = '이메일 중복 확인 중 오류가 발생했습니다.'; // 에러 처리 메시지
+        produce((s) => {
+          s.isEmail = null;
         })
       );
-
       return false;
     }
   };
 
+  const checkPhonNumberDuplicate = async (phoneNumber) => {
+    try {
+      const phoneNumberResult = await getFirstListItem(
+        'users',
+        'phoneNumber',
+        phoneNumber
+      );
+
+      set(
+        produce((s) => {
+          s.isPhoneNumber = !phoneNumberResult;
+        })
+      );
+      return !phoneNumberResult;
+    } catch (error) {
+      console.error('휴대폰 번호 중복확인 실페:', error);
+      set(
+        produce((s) => {
+          s.isPhoneNumber = null;
+        })
+      );
+      return false;
+    }
+  };
   // 프로필을 업데이트하는 함수
-  const updateProfile = async (profileImageFile, newNickname, newEmail) => {
+  const updateProfile = async (
+    profileImageFile,
+    newNickname,
+    newEmail,
+    newPhoneNumber
+  ) => {
     const authData = pb.authStore.model;
 
     if (authData) {
@@ -137,6 +141,7 @@ export const useMyPageStore = create((set) => {
         // 닉네임과 이메일을 FormData에 추가
         formData.append('nickName', newNickname || authData.nickName);
         formData.append('email', newEmail || authData.email);
+        formData.append('phoneNumber', newPhoneNumber || authData.phoneNumber);
 
         // 사용자 정보를 PocketBase에서 업데이트
         const updatedRecord = await updateData('users', authData.id, formData);
@@ -148,6 +153,7 @@ export const useMyPageStore = create((set) => {
                 ...s.userData,
                 nickName: updatedRecord.nickName,
                 email: updatedRecord.email,
+                phoneNumber: updatedRecord.phoneNumber,
                 profileImage: getPbImageURL(
                   updatedRecord.id,
                   updatedRecord.photo
@@ -170,5 +176,6 @@ export const useMyPageStore = create((set) => {
     updateProfile,
     checkNicknameDuplicate,
     checkEmailDuplicate,
+    checkPhonNumberDuplicate,
   };
 });
