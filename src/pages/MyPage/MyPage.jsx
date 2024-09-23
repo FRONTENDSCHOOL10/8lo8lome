cea80c3d4862219b8e97b8091a75e1de20b4b160
heejae <leeheejaelcode@gmail.com>
2024-09-23 20:22:33 +0900

    [버그] 네비게이션 버그 수정

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -22,7 +22,7 @@ function MyPage() {
   return (
     <>
       <AppMeta title="마이페이지" description="마이페이지 페이지입니다" />
-      <AppHeader>마이 페이지</AppHeader>
+      <AppHeader navigation>마이 페이지</AppHeader>
       <MyProfile />
       <MyCoupon />
       <MyInfo />
