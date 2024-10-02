639661b8b68b94bee094daad87fbb3b1d3a56599
heejae <leeheejaelcode@gmail.com>
2024-10-02 17:58:02 +0900

    [생성] fetchGymsList.js 분리

---
+++ src/tanstackQuery/fetchGymsList.js
@@ -0,0 +1,21 @@
+import { getUserLocation, filterGymsByDistance } from '@/utils'; // 필요한 함수들 가져오기
+import { getAllData } from '@/api/CRUD';
+
+// 헬스장 목록을 가져오는 비동기 함수
+export const fetchGymsList = async ({ latitude, longitude }) => {
+  // 서버에서 헬스장 데이터 가져오기
+  const gyms = await getAllData('gyms', '-created');
+
+  // 위치가 없으면 현재 위치로 설정
+  if (!latitude || !longitude) {
+    const userLocation = await getUserLocation();
+    latitude = userLocation.latitude;
+    longitude = userLocation.longitude;
+  }
+
+  // 헬스장 목록 필터링
+  const filteredGyms = await filterGymsByDistance(gyms, latitude, longitude);
+
+  // 필터링된 헬스장 데이터를 반환
+  return { gyms, filteredGyms, latitude, longitude };
+};
