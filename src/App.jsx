f5a21ae23077794adf4e2cb9a2b6189c85375163
heejae <leeheejaelcode@gmail.com>
2024-10-04 17:47:45 +0900

    [리팩토링] defaultOption 설정

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,21 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 // 캐싱 관리 객체
 // eslint-disable-next-line react-refresh/only-export-components
-export const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // 쿼리의 기본 옵션 설정 (예: 자동 리페치, 캐시 유지 시간 등)
+      staleTime: 1000 * 60 * 5, // 5분 동안 데이터가 신선하다고 간주
+      cacheTime: 1000 * 60 * 10, // 10분 동안 캐시 유지
+    },
+    mutations: {
+      onError: (error) => {
+        console.error('Mutation failed:', error);
+      },
+    },
+  },
+});
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
